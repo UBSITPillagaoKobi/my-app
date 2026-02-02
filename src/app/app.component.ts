46aa90eaa2e314f8177d4a1e53d93d88c788f6d7
UBSITPillagaoKobi <20187699@s.ubaguio.edu>
2026-02-02 10:36:16 +0800

    Creaing the navigation aswell as the login form from bootswatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NaviComponent } from './comps/navi/navi.component';
+import { LoginComponent } from './comps/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports:[NaviComponent,LoginComponent],
 })
 export class AppComponent {
   title = 'my-app';
